c579cdb6f699b19db7048e3dc153b6d396b74b6c
sarmueller <sarah@mueteam.de>
2023-08-19 13:30:25 +0200

    cifar dataset, black format

--- src/multicropdataset.py
+++ src/multicropdataset.py
@@ -4,13 +4,16 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
+import os
+import pickle
 import random
 from logging import getLogger
+from typing import Any, Callable, Optional, Tuple
 
-from PIL import ImageFilter
 import numpy as np
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from PIL import Image, ImageFilter
 
 logger = getLogger()
 
@@ -43,13 +46,20 @@ class MultiCropDataset(datasets.ImageFolder):
                 size_crops[i],
                 scale=(min_scale_crops[i], max_scale_crops[i]),
             )
-            trans.extend([transforms.Compose([
-                randomresizedcrop,
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.Compose(color_transform),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=mean, std=std)])
-            ] * nmb_crops[i])
+            trans.extend(
+                [
+                    transforms.Compose(
+                        [
+                            randomresizedcrop,
+                            transforms.RandomHorizontalFlip(p=0.5),
+                            transforms.Compose(color_transform),
+                            transforms.ToTensor(),
+                            transforms.Normalize(mean=mean, std=std),
+                        ]
+                    )
+                ]
+                * nmb_crops[i]
+            )
         self.trans = trans
 
     def __getitem__(self, index):
@@ -61,6 +71,151 @@ class MultiCropDataset(datasets.ImageFolder):
         return multi_crops
 
 
+
+class CIFAR10MultiCrop(datasets.VisionDataset):
+    """`CIFAR10 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
+
+    Args:
+        root (string): Root directory of dataset where directory
+            ``cifar-10-batches-py`` exists or will be saved to if download is set to True.
+        train (bool, optional): If True, creates dataset from training set, otherwise
+            creates from test set.
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        size_crops: Crops resolutions (example: [224, 96]).
+        nmb_crops: List of number of crops (example: [2, 6]).
+        min_scale_crops: Argument in RandomResizedCrop (example: [0.14, 0.05]).
+        max_scale_crops: Argument in RandomResizedCrop (example: [1., 0.14]).
+        color_distortion_strength: Strength of color distortion, default 1.
+        return_labels: If true labels are returned.
+    """
+
+    base_folder = "cifar-10-batches-py"
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    filename = "cifar-10-python.tar.gz"
+    tgz_md5 = "c58f30108f718f92721af3b95e74349a"
+    train_list = [
+        ["data_batch_1", "c99cafc152244af753f735de768cd75f"],
+        ["data_batch_2", "d4bba439e000b95fd0a9bffe97cbabec"],
+        ["data_batch_3", "54ebc095f3ab1f0389bbae665268c751"],
+        ["data_batch_4", "634d18415352ddfa80567beed471001a"],
+        ["data_batch_5", "482c414d41f54cd18b22e5b47cb7c3cb"],
+    ]
+
+    test_list = [
+        ["test_batch", "40351d587109b95175f43aff81a1287e"],
+    ]
+    meta = {
+        "filename": "batches.meta",
+        "key": "label_names",
+        "md5": "5ff9c542aee3614f3951f8cda6e48888",
+    }
+
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        target_transform: Optional[Callable] = None,
+        size_crops: Tuple[int] = [224],
+        nmb_crops: Tuple[int] = [2],
+        min_scale_crops: Tuple[float] = [0.14],
+        max_scale_crops: Tuple[float] = [1],
+        color_distortion_strength: float = 1.,
+        return_labels: bool = False,
+    ) -> None:
+
+        super().__init__(root, transform=None, target_transform=target_transform)
+
+        self.train = train  # training set or test set
+        self.return_labels = return_labels
+
+        if self.train:
+            downloaded_list = self.train_list
+        else:
+            downloaded_list = self.test_list
+
+        self.data: Any = []
+        self.targets = []
+
+        # now load the picked numpy arrays
+        for file_name, checksum in downloaded_list:
+            file_path = os.path.join(self.root, self.base_folder, file_name)
+            with open(file_path, "rb") as f:
+                entry = pickle.load(f, encoding="latin1")
+                self.data.append(entry["data"])
+                if "labels" in entry:
+                    self.targets.extend(entry["labels"])
+                else:
+                    self.targets.extend(entry["fine_labels"])
+
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
+
+        self._load_meta()
+
+        assert len(size_crops) == len(nmb_crops)
+        assert len(min_scale_crops) == len(nmb_crops)
+        assert len(max_scale_crops) == len(nmb_crops)
+
+        color_transform = [
+            get_color_distortion(s=color_distortion_strength), 
+            PILRandomGaussianBlur(),
+        ]
+        mean = [0.491, 0.482, 0.447]
+        std = [0.247, 0.243, 0.262]
+        trans = []
+        for i in range(len(size_crops)):
+            randomresizedcrop = transforms.RandomResizedCrop(
+                size_crops[i],
+                scale=(min_scale_crops[i], max_scale_crops[i]),
+            )
+            trans.extend([transforms.Compose([
+                randomresizedcrop,
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.Compose(color_transform),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=mean, std=std)
+            ])
+            ] * nmb_crops[i])
+        self.trans = trans
+
+    def _load_meta(self) -> None:
+        path = os.path.join(self.root, self.base_folder, self.meta["filename"])
+        with open(path, "rb") as infile:
+            data = pickle.load(infile, encoding="latin1")
+            self.classes = data[self.meta["key"]]
+        self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+        multi_crops = list(map(lambda trans: trans(img), self.trans))
+        if self.return_labels:
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+            return multi_crops, target
+        else:
+            return multi_crops
+
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def extra_repr(self) -> str:
+        split = "Train" if self.train is True else "Test"
+        return f"Split: {split}"
+
+
 class PILRandomGaussianBlur(object):
     """
     Apply Gaussian Blur to the PIL image. Take the radius and probability of
@@ -68,7 +223,7 @@ class PILRandomGaussianBlur(object):
     This transform was used in SimCLR - https://arxiv.org/abs/2002.05709
     """
 
-    def __init__(self, p=0.5, radius_min=0.1, radius_max=2.):
+    def __init__(self, p=0.5, radius_min=0.1, radius_max=2.0):
         self.prob = p
         self.radius_min = radius_min
         self.radius_max = radius_max
@@ -87,7 +242,7 @@ class PILRandomGaussianBlur(object):
 
 def get_color_distortion(s=1.0):
     # s is the strength of color distortion.
-    color_jitter = transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)
+    color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
     rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
     rnd_gray = transforms.RandomGrayscale(p=0.2)
     color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
